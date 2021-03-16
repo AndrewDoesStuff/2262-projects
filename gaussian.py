3b8aca36d8c3d4e3bf3cfefb5b589e60405526e2
Andrew <ahhannie56@gmail.com>
2021-03-16 17:53:32 -0500

    added a comment

--- gaussian.py
+++ gaussian.py
@@ -1,4 +1,9 @@
 import numpy as np
+'''
+    I got a D+ on this b/c i used a numpy function lol
+
+    Cool, man!
+'''
 def main():
     a = np.array([[2,1,-1,-2],[4,4,1,3],[-6,-1,10,10],[2,1,8,4]])
     b = np.array([2,4,-5,1])
